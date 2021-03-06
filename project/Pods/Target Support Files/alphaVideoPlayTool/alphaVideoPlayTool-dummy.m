6ccc34e4e06e00248fe3fd141471931545ce0ddd
huweiwei <935932000@qq.com>
2021-03-06 13:50:38 +0800

    0.0.2

---
+++ project/Pods/Target Support Files/alphaVideoPlayTool/alphaVideoPlayTool-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_alphaVideoPlayTool : NSObject
+@end
+@implementation PodsDummy_alphaVideoPlayTool
+@end
--- project/project/ViewController.m
+++ project/project/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <SLMaskVideoPlayerLayer.h>
 
 @interface ViewController ()
 
